e22b8fb5eba55140386c8284cc223b7ac6de28cd
Carlos Rossy <carlosrossy@bemol.com.br>
2025-11-04 13:39:50 -0400

    ajuste de router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // <-- sem BrowserRouter aqui
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </TooltipProvider>
 );
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryProvider } from "./providers/QueryProvider";
 
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-    <QueryClientProvider client={queryClient}>
+    <QueryProvider>
         <BrowserRouter basename="/pontos_mais">
             <App />
         </BrowserRouter>
-    </QueryClientProvider>
+    </QueryProvider>
 );
